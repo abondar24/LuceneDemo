591e1dc131318f3d4d98046ce0b0fa0990b97d15
abondar24 <abondar1992@gmail.com>
2017-03-13 22:37:41 -0700

    Project "chassis"

---
+++ src/main/java/org/abondar/experimental/lucenedemo/Main.java
@@ -0,0 +1,11 @@
+package org.abondar.experimental.lucenedemo;
+
+/**
+ * Created by abondar on 3/13/17.
+ */
+public class Main {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
