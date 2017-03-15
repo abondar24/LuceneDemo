ed5fb529452c5292c6c62af0298531d0f8c5e4a7
abondar24 <abondar1992@gmail.com>
2017-03-14 21:17:24 -0700

    Basic Indexer and Searcher

---
+++ src/main/java/org/abondar/experimental/lucenedemo/basics/Indexer.java
@@ -0,0 +1,81 @@
+package org.abondar.experimental.lucenedemo.basics;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Date;
+
+public class Indexer {
+    public static void main(String[] args) throws Exception {
+
+        File indexDir = new File("/home/abondar/Doucments");
+        File dataDir = new File("/home/abondar/android-sdk-linux");
+
+        long start = new Date().getTime();
+        int numIndexed = index(indexDir, dataDir);
+        long end = new Date().getTime();
+
+        System.out.println("Indexing " + numIndexed + " files took "
+                + (end - start) + " milliseconds");
+
+    }
+
+    private static int index(File indexDir, File dataDir) throws IOException {
+        if (!dataDir.exists() || !dataDir.isDirectory()) {
+            throw new IOException(dataDir + "does not exist or not a directory");
+        }
+
+
+        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+        Directory indexDirectory = FSDirectory.open(indexDir.toPath());
+        IndexWriter writer = new IndexWriter(indexDirectory, config);
+
+        indexDir(writer, dataDir);
+
+        int numIndexed = writer.numDocs();
+
+        writer.close();
+        return numIndexed;
+    }
+
+    private static void indexDir(IndexWriter writer, File dataDir) throws IOException {
+
+        File[] files = dataDir.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
+            if (f.isDirectory()) {
+                indexDir(writer, f);
+            } else if (f.getName().endsWith(".txt")){
+                indexFile(writer,f);
+            }
+        }
+    }
+
+    private static void indexFile(IndexWriter writer, File f) throws IOException {
+        if (f.isHidden() || !f.exists() || !f.canRead()){
+            return;
+        }
+
+        System.out.println("Indexing " + f.getCanonicalPath());
+
+        Document document = new Document();
+        document.add(new TextField("contents", new FileReader(f)));
+        document.add(new StringField("filename", f.toString(), Field.Store.YES));
+        writer.addDocument(document);
+    }
+
+
+}
---
+++ src/main/java/org/abondar/experimental/lucenedemo/basics/Searcher.java
@@ -0,0 +1,57 @@
+package org.abondar.experimental.lucenedemo.basics;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
+import java.io.File;
+import java.util.Date;
+
+
+public class Searcher {
+
+    public static void main(String[] args) throws Exception {
+        File indexDir = new File("/home/abondar/Doucments");
+        String q = "intent";
+
+        if (!indexDir.exists() || !indexDir.isDirectory()) {
+            throw new Exception(indexDir + "does not exist or is not a directory");
+        }
+
+        search(indexDir, q);
+    }
+
+    private static void search(File indexDir, String q) throws Exception {
+        Directory fsDir = FSDirectory.open(indexDir.toPath());
+        IndexReader indexReader = DirectoryReader.open(fsDir);
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+        QueryParser queryParser = new QueryParser("contents", new StandardAnalyzer());
+        Query query = queryParser.parse(q);
+
+        long start = new Date().getTime();
+
+        int hitsPerPage = 10;
+        TopDocs results = indexSearcher.search(query, 5 * hitsPerPage);
+
+        long end = new Date().getTime();
+
+        ScoreDoc[] hits = results.scoreDocs;
+        System.out.println("Found " + results.totalHits +
+                " document(s) (in " + (end - start) +
+                " milliseconds) that matched query " +
+                q + ":");
+
+
+
+        for (int i=0; i<results.totalHits;i++){
+            Document document = indexSearcher.doc(hits[i].doc);
+            System.out.println(document.get("filename"));
+        }
+    }
+}
